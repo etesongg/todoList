5fc3113eb2b257e7dcc233dc5094c4f757a42fd8
etesong014 <etesong0114@gmail.com>
2024-02-15 17:01:46 +0900

    - 각각의 filter에 따라서 아이템 필터링하기 - cursor 스타일 변경하기

--- main.js
+++ main.js
@@ -12,8 +12,18 @@
 
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
-let taskList = []
-addButton.addEventListener("click", addTask)
+let tabs = document.querySelectorAll(".task-taps div");
+let taskList = [];
+let mode = "all";
+let filterList = [];
+
+addButton.addEventListener("click", addTask);
+
+for(let i=1; i<tabs.length; i++){
+    tabs[i].addEventListener("click", function (event) {
+        filter(event);
+    });
+}
 
 function addTask() {
     let task = {
@@ -25,31 +35,38 @@ function addTask() {
         window.alert("내용을 입력해주세요.")
     }else{
     taskList.push(task);
-    console.log(taskList)
     render()
     }
 }
 
 function render() {
+    // 1. 내가 선택한 탭에 따라서
+    // 2. 리스트를 달리 보여준다
+    let list = []
+    if(mode === "all"){
+        list = taskList
+    }else if(mode == "ongoing" || mode === "done"){
+        list = filterList
+    }
     let resultHTML = '';
 
-    for(let i=0; i<taskList.length; i++) {
-        if(taskList[i].isComplete == true) {
+    for(let i=0; i<list.length; i++) {
+        if(list[i].isComplete == true) {
             resultHTML +=`
             <div class="task">
-                <div class="task-done">${taskList[i].taskContent}</div>
+                <div class="task-done">${list[i].taskContent}</div>
                 <div>
-                    <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-                    <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+                    <button onclick="toggleComplete('${list[i].id}')">Check</button>
+                    <button onclick="deleteTask('${list[i].id}')">Delete</button>
                 </div>
             </div>`;
         }else {
             resultHTML += `
             <div class="task">
-                <div>${taskList[i].taskContent}</div>
+                <div>${list[i].taskContent}</div>
                 <div>
-                    <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-                    <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+                    <button onclick="toggleComplete('${list[i].id}')">Check</button>
+                    <button onclick="deleteTask('${list[i].id}')">Delete</button>
                 </div>
             </div>`;
         }  
@@ -78,6 +95,29 @@ function deleteTask(id){
     }
     render()
 }
+
+function filter(event){
+    mode = event.target.id
+    filterList = []
+    if(mode === "all"){
+        render()
+    }else if(mode === "ongoing"){
+        for(let i=0; i<taskList.length; i++){
+            if(taskList[i].isComplete == false){
+                filterList.push(taskList[i])
+            }
+        }
+        render();
+    }else if(mode === "done"){
+        for(let i=0; i<taskList.length; i++){
+            if(taskList[i].isComplete == true){
+                filterList.push(taskList[i])
+            }
+        }
+        render();
+    }
+}
+
 
 function randomIDGenerate() {
     return '_' + Math.random().toString(36).substr(2, 9);
